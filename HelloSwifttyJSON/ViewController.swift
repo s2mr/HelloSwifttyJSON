3518b9555344cd5cc07cd5bac8120bd804088946
下村一将 <kazumasa@shimomuraisshou-no-macbook-air.local>
2016-02-06 15:06:20 +0900

    Initial Commit

---
+++ HelloSwifttyJSON/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloSwifttyJSON
+//
+//  Created by 下村一将 on 2016/02/06.
+//  Copyright © 2016年 kaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
